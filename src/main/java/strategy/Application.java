8ca5f091fb60d72815ea1a16dc3a9ae209c87c57
palraj <palrajannadurai@gmail.com>
2026-03-13 05:24:56 +0530

    strategy pattern applications changes

--- src/main/java/strategy/Application.java
+++ src/main/java/strategy/Application.java
@@ -1,14 +1,18 @@
 package strategy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
     public static void main(String[] args) {
+        Map<MathematicalOperations, Strategy> strategies = new HashMap<>();
+        strategies.put(MathematicalOperations.ADD, new ConcreteStrategyAdd());
+        strategies.put(MathematicalOperations.SUBTRACT, new ConcreteStrategySubtract());
+
+
         Context context = new Context();
-        MathematicalOperations action = MathematicalOperations.ADD;
-        if (action == MathematicalOperations.ADD) {
-            context.setStrategy(new ConcreteStrategyAdd());
-        } else if (action == MathematicalOperations.SUBTRACT) {
-            context.setStrategy(new ConcreteStrategySubtract());
-        }
+        MathematicalOperations action = MathematicalOperations.SUBTRACT;
+        context.setStrategy(strategies.get(action));
         int a = 10, b = 20;
         System.out.println(context.executeStrategy(a, b));
     }
