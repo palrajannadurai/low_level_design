f704a31e8c7b0710e6bb6c43572ad91d50bef82e
palraj <palrajannadurai@gmail.com>
2026-03-16 22:12:09 +0530

    Restructuring

--- design_patterns_and_lld_problems/stock_price_alert_system/Observer.java
+++
@@ -1,5 +0,0 @@
-package stock_price_alert_system;
-
-public interface Observer {
-    void update(Stock stock);
-}
--- design_patterns_and_lld_problems/stock_price_alert_system/Subject.java
+++
@@ -1,9 +0,0 @@
-package stock_price_alert_system;
-
-public interface Subject {
-    void addObserver(Observer observer);
-
-    void removeObserver(Observer observer);
-
-    void notifyObservers();
-}
